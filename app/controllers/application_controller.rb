73f1cef20b292778934464542f54620afc5d610e
Corrina Calanoc <corrina.calanoc@berkeley.edu>
2016-03-10 11:13:42 -0800

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,23 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_filter :get_trainings
+  def get_trainings
+  	db = Mdb.open("/Users/corrinasthename/Desktop/cs169/BCGC.mdb")
+    @trainings = db[:tblTraining]
+    # @administration = Administration.first
+  end
+  # database = Mdb.open('/Users/corrinasthename/Desktop/cs169/BCGC.mdb')
+  # training_orders = database[:tblOrders]
+  # trainings = database[:tblTraining]
+
+  # def index
+  #   db = Mdb.open("/Users/corrinasthename/Desktop/cs169/BCGC.mdb")
+  #   output = "Tables: #{db.tables}"
+  #   output += "<br/><br/>"
+  #   output += "Data: #{db[:tblOrders].first}"
+  #   render text: output
+  # end
+end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,62 @@
+class HomeController < ApplicationController
+  
+# title, training type, location, date(day
+
+# STC sign in sheet: 
+# Certification number, 
+# certified hours, 
+# date certified, 
+# course title, 
+# training provider, 
+# telephone number, 
+# instructors
+
+  def index
+    # db = Mdb.open("/Users/corrinasthename/Desktop/cs169/BCGC.mdb")
+    # trainings = db[:tblTraining]
+    type = @trainings.first[:Type]
+    start_date = @trainings.first[:DateStart]
+    end_date = @trainings.first[:DateEnd]
+    location = "#{@trainings.first[:City]}"+", "+"{@trainings.first[:State]}"+"{@trainings.first[:ZipCode]}"
+
+    output = "Training Data: #{@trainings.first}"
+    output += "<br/><br/>"
+    output += "Type: #{type}<br/><br/>"
+    output += "Dates: #{@trainings.first[:DateStart]}, #{@trainings.first[:DateEnd]}"
+    output += "<br/><br/>"
+    output += "Location: #{@trainings.first[:City]}"+", "+"#{@trainings.first[:State]}"+" "+"#{@trainings.first[:ZipCode]}"
+    render text: output
+  end
+
+  def cert_num
+  	# training_id = params[:training_id]
+  	db = Mdb.open("/Users/corrinasthename/Desktop/cs169/BCGC.mdb")
+    trainings = db[:tblTraining]
+  end
+
+  def cert_hrs
+  	# training_id = params[:training_id]
+  	# @training.
+  end
+
+  def cert_date
+  	# training_id = params[:training_id]
+  	@training[training_id][:DateStart]
+  end
+
+  def course_title
+  end
+
+  def training_provider
+  	#always OneCircleFoundation?
+  end
+
+  def telephone_number
+  	#one circle's phone number
+  end
+
+  def instructors
+  	
+  end
+
+end
